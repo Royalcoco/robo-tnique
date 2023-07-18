62013a8f9e65b96185cc1c0feb97b3abe82ced0a
Royalcoco <134060194+Royalcoco@users.noreply.github.com>
2023-07-18 17:03:46 +0200

    first commit

---
+++ -- Déclaration des variables et initiali.lua
@@ -0,0 +1,22 @@
+-- Déclaration des variables et initialisation
+local vitesse = 0
+local distance = 0
+
+-- Fonction pour déplacer le robot
+function deplacerRobot(v, d)
+  print("Déplacement du robot...")
+  -- Logique de déplacement du robot en fonction de la vitesse (v) et de la distance (d)
+  -- À compléter selon les spécifications du robot
+  -- function deplacerRobot(v, d)
+--   print("Déplacement du robot...")
+-- Exemple : déplacer le robot à une vitesse donnée sur une distance donnée
+end
+
+-- Appel de la fonction de déplacement du robot avec des valeurs spécifiques
+deplacerRobot(10, 100)
+
+-- local vitesse = 0
+-- local distance = 0
+
+-- end
+
---
+++ cmd 01.lua
@@ -0,0 +1,128 @@
+-- Ouvre la command "tier-utile' de la command table a la command Affichage, affiche le "menu" ; TaskCombatHatedTargetsInArea( v'yx, x, y, z, radius, p5)
+
+  function (portrait'variant resus de fichiers User"import'-(:Ouvre)-'commande'Affichage'Affiche'menu')
+    
+end
+-- Compare this snippet from -- Déclaration des variables et initial.lua:
+
+{'-task complet combat', 'TaskCombatHatedTargetsInArea', {v'yx', 'x', 'y', 'z', 'radius', 'p5'}},
+
+-- -- Déclaration des variables et initialisation
+('-task'user open file "engaged-foc'user-file'import'open'commande'Affichage'Affiche'menu")
+
+('Ouvre'user open file "engaged- GetDesObjectAnimProgress(desObjectHandle)user-file'import'open'commande'Affichage'Affiche'menu")
+
+('Ouvre'user open file "engaged- GetDesObjectAnimProgress(desObjectHandle)user-file'import'open'commande'Affichage'Affiche'menu")
+
+('Ouvre'user open file "engaged- GetDesObjectAnimProgress(desObjectHandle)user-file'import'open'commande'Affichage'Affiche'menu")
+
+('Ouvre'user open file "engaged- GetDesObjectAnimProgress(desObjectHandle)user-file'import'open'commande'Affichage'Affiche'menu")
+
+('Ouvre'user open file "engaged- GetDesObjectAnimProgress(desObjectHandle)user-file'import'open'commande'Affichage'Affiche'menu")
+
+('Ouvre'user open file "engaged- GetDesObjectAnimProgress(desObjectHandle)user-file'import'open'commande'Affichage'Affiche'menu")
+
+('Ouvre'user open file "engaged- GetDesObjectAnimProgress(desObjectHandle)user-file'import'open'commande'Affichage'Affiche'menu")
+
+('Ouvre'user open file "engaged- GetDesObjectAnimProgress(desObjectHandle)user-file'import'open'commande'Affichage'Affiche'menu")
+
+('Ouvre'user open file "engaged- GetDesObjectAnimProgress(desObjectHandle)user-file'import'open'commande'Affichage'Affiche'menu")
+
+NetworkEnableMotionDrugged() -- Enable motion while drugged
+NetworkEnableMotionDrugged() -- Enable motion while drugged
+NetworkEnableMotionDrugged() -- Enable motion while drugged
+NetworkEnableMotionDrugged() -- Enable motion while drugged
+NetworkEnableMotionDrugged() -- Enable motion while drugged
+NetworkEnableMotionDrugged() -- Enable motion while drugged
+NetworkEnableMotionDrugged() -- Enable motion while drugged
+NetworkEnableMotionDrugged() -- Enable motion while drugged
+NetworkEnableMotionDrugged() -- Enable motion while drugged
+NetworkEnableMotionDrugged() -- Enable motion while drugged
+
+-- -- Déclaration des variables et initialisation
+
+Definie = false
+Definie = false
+Definie = false
+Definie = false
+Definie = false
+Definie = false
+Definie = false
+Definie = false
+Definie = false
+Definie = false
+
+Open :'choic'en func() 'choic'open'commande'Affichage'Affiche'menu'
+Open :'choic'en func() 'choic'open'commande'Affichage'Affiche'menu'
+Open :'choic'en func() 'choic'open'commande'Affichage'Affiche'menu'
+Open :'choic'en func() 'choic'open'commande'Affichage'Affiche'menu'
+Open :'choic'en func() 'choic'open'commande'Affichage'Affiche'menu'
+Open :'choic'en func() 'choic'open'commande'Affichage'Affiche'menu'
+Open :'choic'en func() 'choic'open'commande'Affichage'Affiche'menu'
+Open :'choic'en func() 'choic'open'commande'Affichage'Affiche'menu'
+Open :'choic'en func() 'choic'open'commande'Affichage'Affiche'menu'
+Open :'choic'en func() 'choic'open'commande'Affichage'Affiche'menu'
+
+Find :'choic'DoesScriptedCoverPointExistAtCoords(x, y, z) , 'choic'Find'commande'Affichage'Affiche'menu'
+Follow path with :'choic' {'-task complet combat', 'TaskCombatHatedTargetsInArea', {v'yx', 'x', 'y', 'z', 'radius', 'p5'}}
+Follow path with :'choic' {'-task complet combat', 'TaskCombatHatedTargetsInArea', {v'yx', 'x', 'y', 'z', 'radius', 'p5'}}
+Follow path with :'choic' {'-task complet combat', 'TaskCombatHatedTargetsInArea', {v'yx', 'x', 'y', 'z', 'radius', 'p5'}}
+Follow path with :'choic' {'-task complet combat', 'TaskCombatHatedTargetsInArea', {v'yx', 'x', 'y', 'z', 'radius', 'p5'}}
+Follow path with :'choic' {'-task complet combat', 'TaskCombatHatedTargetsInArea', {v'yx', 'x', 'y', 'z', 'radius', 'p5'}}
+Follow path with :'choic' {'-task complet combat', 'TaskCombatHatedTargetsInArea', {v'yx', 'x', 'y', 'z', 'radius', 'p5'}}
+Follow path with :'choic' {'-task complet combat', 'TaskCombatHatedTargetsInArea', {v'yx', 'x', 'y', 'z', 'radius', 'p5'}}
+Follow path with :'choic' {'-task complet combat', 'TaskCombatHatedTargetsInArea', {v'yx', 'x', 'y', 'z', 'radius', 'p5'}}
+Follow path with :'choic' {'-task complet combat', 'TaskCombatHatedTargetsInArea', {v'yx', 'x', 'y', 'z', 'radius', 'p5'}}
+Follow path with :'choic' {'-task complet combat', 'TaskCombatHatedTargetsInArea', {v'yx', 'x', 'y', 'z', 'radius', 'p5'}}
+
+-- -- Déclaration des variables de l'environnement du IsSystemUiBeingDisplayed() et initialisation
+
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+
+-- ForcePedTo activ'affichage tête-haute et deverouille les capteur de résistance du vsyxhealth
+
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+ForcePedToOpenParachute(ped) -- ForcePedToOpenParachute(ped)
+
+-- -- Déclaration des variables de l'environnement du IsSystemUiBeingDisplayed() et initialisation
+
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+IsSystemUiBeingDisplayed() -- IsSystemUiBeingDisplayed()
+
+
